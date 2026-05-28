30dfd93d82f26fff36f991aaef163af5ee3187a5
Sanjeev Midda <s.midda1@gmail.com>
2026-05-28 21:11:59 +0100

    feat: display todo items in UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import "./index.css";
+import { data } from "./data/data";
+import Task from "./components/Task";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="todoListItems">
+        {data.map((item) => {
+          return <Task taskName={item.taskName} checked={item.checked} />;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
