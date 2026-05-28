be47600fa6f2a7e282a9d8a159ec23e47f91d3d6
Sanjeev Midda <s.midda1@gmail.com>
2026-05-28 20:47:15 +0100

    feat: create todo list data

---
+++ src/data/data.tsx
@@ -0,0 +1,27 @@
+export const data = [
+  {
+    id: 0,
+    taskName: "task one",
+    checked: false,
+  },
+  {
+    id: 2,
+    taskName: "task two",
+    checked: false,
+  },
+  {
+    id: 3,
+    taskName: "task three",
+    checked: false,
+  },
+  {
+    id: 4,
+    taskName: "task four",
+    checked: false,
+  },
+  {
+    id: 5,
+    taskName: "task five",
+    checked: false,
+  },
+];
